8601db6bf9215b90dcd7b05a504ccb67c4c9b9e3
Oscar Bernal <odbg2013@gmail.com>
2022-07-04 18:14:54 -0500

    fcc logo style

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import './App.css';
-import freeCodeCampLogo from './img/freecodecamp-logo.png'
+import freeCodeCampLogo from './img/freecodecamp-logo.png';
 
 function App() {
   return (
-  <div className='App'>
-    <div className="freecodecamp-logo-contenedor">
-      <img 
-        src={freeCodeCampLogo}
-        className=""
-        alt="" />
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img src={freeCodeCampLogo} className='freecodecamp-logo' alt='' />
+      </div>
     </div>
-  </div>
   );
 }
 
