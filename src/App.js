00cf993d90e883df38c6f91fd2c4c25cd361cc1b
Oscar Bernal <odbg2013@gmail.com>
2022-07-04 16:24:53 -0500

    logo freeCodeCamp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import freeCodeCampLogo from './img/freecodecamp-logo.png'
 
 function App() {
-  return <div className='App'>
-    <h1>Hola</h1>
-  </div>;
+  return (
+  <div className='App'>
+    <div className="freecodecamp-logo-contenedor">
+      <img 
+        src={freeCodeCampLogo}
+        className=""
+        alt="" />
+    </div>
+  </div>
+  );
 }
 
 export default App;
